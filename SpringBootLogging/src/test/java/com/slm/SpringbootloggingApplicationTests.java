72ae3dc86d020f433854aad11b5b990b549707be
${huaidanmu} <${1301218756@qq.com}>
2021-01-12 11:25:14 +0800

    日志

---
+++ SpringBootLogging/src/test/java/com/slm/SpringbootloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootloggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
